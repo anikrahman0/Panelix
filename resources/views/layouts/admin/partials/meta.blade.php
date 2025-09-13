02732c678dc13fec2df7994b9d2ef99de209a884
Md. Anik Rahman <a7604366@gmail.com>
2025-09-13 12:18:15 +0600

    panelix config changed to admin

--- resources/views/layouts/admin/partials/meta.blade.php
+++ resources/views/layouts/admin/partials/meta.blade.php
@@ -2,13 +2,13 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<meta name="description" content="{{ $description ?? config('panelix.meta.description') }}">
-<meta name="author" content="{{ $author ?? config('panelix.meta.author') }}">
-<meta name="developer" content="{{ $developer ?? config('panelix.meta.developer') }}">
-<meta name="contact" content="{{ $contact ?? config('panelix.meta.contact') }}">
-<meta name="copyright" content="Copyright (c) {{ now()->year }}. All Rights &reg; Reserved by {{ $copyright ?? config('panelix.meta.copyright') }}.">
+<meta name="description" content="{{ $description ?? config('admin.meta.description') }}">
+<meta name="author" content="{{ $author ?? config('admin.meta.author') }}">
+<meta name="developer" content="{{ $developer ?? config('admin.meta.developer') }}">
+<meta name="contact" content="{{ $contact ?? config('admin.meta.contact') }}">
+<meta name="copyright" content="Copyright (c) {{ now()->year }}. All Rights &reg; Reserved by {{ $copyright ?? config('admin.meta.copyright') }}.">
 
-<meta name="robots" content="{{ $robots ?? config('panelix.meta.robots') }}">
-<meta name="googlebot" content="{{ $googlebot ?? config('panelix.meta.googlebot') }}">
-<meta name="googlebot-news" content="{{ $googlebotNews ?? config('panelix.meta.googlebot_news') }}">
-<meta name="msnbot" content="{{ $msnbot ?? config('panelix.meta.msnbot') }}">
+<meta name="robots" content="{{ $robots ?? config('admin.meta.robots') }}">
+<meta name="googlebot" content="{{ $googlebot ?? config('admin.meta.googlebot') }}">
+<meta name="googlebot-news" content="{{ $googlebotNews ?? config('admin.meta.googlebot_news') }}">
+<meta name="msnbot" content="{{ $msnbot ?? config('admin.meta.msnbot') }}">
